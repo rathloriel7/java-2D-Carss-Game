9cb52466aa24e16fd1ec96c8fe6604053f18ca05
rathloriel7 <75047479+rathloriel7@users.noreply.github.com>
2023-01-12 11:37:43 +0300

    Update work.java

--- src/cars/work.java
+++ src/cars/work.java
@@ -19,7 +19,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import ja/*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
